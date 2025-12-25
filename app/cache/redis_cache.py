7a13e8fc2aae6a17b36b0bd92bdc56c5b2029d05
shweta yadav <shwetarayadav@gmail.com>
2025-12-25 18:49:51 +0530

    solved json error

--- app/cache/redis_cache.py
+++ app/cache/redis_cache.py
@@ -8,8 +8,8 @@ redis_client = redis.Redis.from_url(settings.REDIS_URL)
 def get_cached_prediction(key:str):
     value = redis_client.get(key)
     if value:
-        return json.loads(value)
+        return eval(value)
     return None
 
 def set_cached_prediction(key:str,value:dict,exp:int=3600):
-    redis_client.setex(key,exp,json.dump(value))
+    redis_client.set(key,str(value))
