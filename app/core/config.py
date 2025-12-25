2c4dcb7fc9dcabd5320af645ef1b62c2ff866042
shweta yadav <shwetarayadav@gmail.com>
2025-12-25 15:06:43 +0530

    alogithm code changed

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings:
     PROJECT_NAME = "Car Price API"
     API_KEY = os.getenv("API_KEY","demo-key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY","secret")
-    JWT_ALGORITHM = "H256"
+    JWT_ALGORITHM = "HS256"
     REDIS_URL = os.getenv("REDIS_URL","redis://localhost:6379")
     MODEL_PATH = "app/models/model.joblib"
 
