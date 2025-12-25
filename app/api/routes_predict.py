a1aea54587f5d9b2fb5f57633442ff59dd5f7b2a
shweta yadav <shwetarayadav@gmail.com>
2025-12-25 18:36:50 +0530

    changes made to files

--- app/api/routes_predict.py
+++ app/api/routes_predict.py
@@ -6,23 +6,22 @@ from app.services.model_services import predict_car_price
 router = APIRouter()
 
 class CarFeature(BaseModel):
-    Levy : float                
-    Year :  int  
-    Category: str 
-    Manufacturer:str
-    LeatherInterior : str 
-    FuelType : str 
-    EngineVolume : float
-    Mileage : int
-    Cylinders : float
-    GearBoxType : str 
-    DriveWheels : str 
-    Doors : int
-    Wheel : str 
-    Color : str 
-    Turbo: str
-    Airbags : int              
+    levy : float                
+    prod_year :  int  
+    category: str 
+    manufacturer:str
+    leather_interior : bool
+    fuel_type : str 
+    engine_volume : float
+    mileage : int
+    cylinders : float
+    gear_box_type : str 
+    drive_wheels : str 
+    doors : int
+    wheel : str 
+    color : str 
+    airbags : int              
 @router.post('/predict')
 def predict_price(car_feat:CarFeature,user=Depends(get_current_user), _ = Depends(get_api_key)):
     predicted_price = predict_car_price(car_feat.model_dump())
-    return {"Predicted Car Price" :f":{predicted_price:.2f}"}
+    return {"Predicted Car Price" :f":{predicted_price:,.2f}"}
--- training/train_model.py
+++ training/train_model.py
@@ -12,30 +12,42 @@ from sklearn.ensemble import RandomForestRegressor
 from training.train_utils import DATA_FILE_PATH,MODEL_DIR,MODEL_PATH
 
 
-data = pd.read_csv('data/car_price_prediction.csv').drop_duplicates().drop(['ID','Model'],axis=1)
+data = pd.read_csv(DATA_FILE_PATH).drop_duplicates().drop(['ID','Model'],axis=1)
 
-data = data.rename(columns={'Prod. year':'Year',
-                            'Engine volume':'EngineVolume',
-                            'Leather interior':'LeatherInterior',
-                            'Fuel type':'FuelType',
-                             'Gear box type': 'GearBoxType',
-                             'Drive wheels':'DriveWheels',
+data = data.rename(columns={'Price':'price',
+                            'Levy':'levy',
+                            'Prod. year':'prod_year',
+                            'Engine volume':'engine_volume',
+                            'Mileage':'mileage',
+                            'Cylinders':'cylinders',
+                            'Doors':'doors',
+                            'Airbags':'airbags',
+                            'Manufacturer':'manufacturer',
+                            'Leather interior':'leather_interior',
+                            'Category':'category',
+                            'Fuel type':'fuel_type',
+                             'Gear box type': 'gear_box_type',
+                             'Drive wheels':'drive_wheels',
+                             'Wheel':'wheel',
+                             'Color':'color'
                              })
 
-data['Levy'] = data['Levy'].replace({'-':np.nan}).astype(float)
-data['LeatherInterior'] = data['LeatherInterior'].replace({'Yes':True,'No':False})
-data['Mileage'] = data['Mileage'].str.extract(r'([\d.]+)').astype(int)
-data['Doors'] = data['Doors'].replace({'04-May':4, '02-Mar':2, '>5':5})
+data['levy'] = data['levy'].replace({'-':np.nan}).astype(float)
+data['leather_interior'] = data['leather_interior'].replace({'Yes':True,'No':False})
+data['mileage'] = data['mileage'].str.extract(r'([\d.]+)').astype(int)
+data['doors'] = data['doors'].replace({'04-May':4, '02-Mar':2, '>5':5})
 
-data['EngineVolume'] = data['EngineVolume'].str.lower()
-data['Turbo'] = data['EngineVolume'].str.contains('turbo')
-data['EngineVolume'] = data['EngineVolume'].str.extract(r'([\d.+])').astype(float)
-
-num_feat = ['Levy', 'Year','EngineVolume', 'Mileage', 'Cylinders', 'Doors', 'Airbags']
-cat_feat = ['Manufacturer', 'LeatherInterior','Category', 'FuelType', 'GearBoxType',
-       'DriveWheels', 'Wheel', 'Color','Turbo']
+# data['engine_volume'] = data['engine_volume'].str.lower()
+# data['turbo'] = data['engine_volume'].str.contains('turbo')
+data['engine_volume'] = data['engine_volume'].str.extract(r'([\d.+])').astype(float)
 
 
+
+num_feat = ['levy', 'prod_year','engine_volume', 'mileage', 'cylinders', 'doors', 'airbags']
+cat_feat = ['manufacturer', 'leather_interior','category', 'fuel_type', 'gear_box_type',
+       'drive_wheels', 'wheel', 'color']
+
+print(list(zip(data.columns ,data.dtypes)))
 num_pipeline = Pipeline([
     ('imputer',SimpleImputer(strategy='median')),
     ('std_scaler',StandardScaler())
@@ -48,7 +60,7 @@ pipeline = ColumnTransformer([
 ])
 
 
-y = data['Price']
+y = data['price']
 X_train,X_test,y_train,y_test = train_test_split(data,y,test_size=0.3,random_state=42)
 
 rf_reg = RandomForestRegressor(max_features=33, n_estimators=200, random_state=42)
